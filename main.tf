b10aca6e68f748d52c06daba6174f2c2abc3da1b
Abdulmajid Adesokan <abdulmajidadesokan@Abdulmajids-MacBook-Air.local>
2022-03-12 15:31:27 +0100

    First commit

---
+++ main.tf
@@ -0,0 +1,6 @@
+module "networking" {
+  source               = "./networking"
+  vpc_cidr             = var.vpc_cidr
+  public_subnet_cidrs  = var.public_subnet_cidrs
+  private_subnet_cidrs = var.private_subnet_cidrs
+}
